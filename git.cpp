38b000dc5528bc3ed4c0b4403bb1f338b91b53da
jahed-hub <jahedtamboli8459@gmail.com>
2025-09-18 11:04:12 +0530

    veriable added

--- git.cpp
+++ git.cpp
@@ -5,7 +5,13 @@ int main () {
 
     cout << "hello world\n";
     cout << "apna collage\n";
+    int x ;
+    cout << "enter x : ";
+    cin >> x;
+    cout << "you entered : " << x << endl;
     
+
+
 
     return 0;
 
---
+++ hello/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a = 12, b = 10;
+    int sun  = a + b;
+    cout << sun << endl;
+    int x;
+    cout << "enter x : " ;
+    cin >> x ;
+
+    
+    
+    return 0;
+
+}
